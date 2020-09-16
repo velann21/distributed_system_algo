7088e83e407c8bb30b71a3c5ad59e6685aad07a6
velann21 <velan@reynencourt.com>
2020-09-16 20:14:35 +0200

    Added the service registry full proof code

--- service_discovery_algo/service_discovery.go
+++ service_discovery_algo/service_discovery.go
@@ -7,97 +7,120 @@ import (
 	"time"
 )
 
-const SERVICEREGISTERY  =  "/service_registry"
+const SERVICEREGISTERY = "/service_registry"
+
 type ServiceRegistry struct {
-	zkConn *zk.Conn
-	addressList []string
+	zkConn          *zk.Conn
+	addressList     []string
 	eventchangeLogs chan []string
-	wait chan bool
+	wait            chan bool
 }
 
 type SRMetaData struct {
 	Endpoint string
 }
 
-func NewServiceRegistry()*ServiceRegistry{
+func NewServiceRegistry() *ServiceRegistry {
 	addressList := make([]string, 0)
 	eventchangeLogs := make(chan []string, 0)
 	wait := make(chan bool)
-	return &ServiceRegistry{addressList:addressList, eventchangeLogs:eventchangeLogs, wait:wait}
+	return &ServiceRegistry{addressList: addressList, eventchangeLogs: eventchangeLogs, wait: wait}
 }
 
-
 type PATH string
-func (sd *ServiceRegistry) CreateServiceRegistryZnode()(PATH, error){
+
+func (sd *ServiceRegistry) CreateServiceRegistryZnode() (PATH, error) {
 	var fullPathString PATH
 	exist, _, err := sd.zkConn.Exists(SERVICEREGISTERY)
-	if err != nil{
+	if err != nil {
 		return fullPathString, err
 	}
-	if exist{
+	if exist {
 		return fullPathString, nil
 	}
 	fullPath, err := sd.zkConn.Create(SERVICEREGISTERY, []byte{}, 0, zk.WorldACL(zk.PermAll))
-	if err != nil{
+	if err != nil {
 		return fullPathString, err
 	}
 	fullPathString = PATH(fullPath)
 	return fullPathString, nil
 }
 
-
-func (sd *ServiceRegistry) RegisterToCluster(meta SRMetaData)(PATH, error){
+func (sd *ServiceRegistry) RegisterToCluster(meta SRMetaData) (PATH, error) {
 	var fullPathString PATH
 	metaByte, err := json.Marshal(meta)
-	if err != nil{
+	if err != nil {
 		return fullPathString, err
 	}
-	fullPath, err := sd.zkConn.Create(SERVICEREGISTERY, metaByte, zk.FlagSequence|zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
-	if err != nil{
+	fullPath, err := sd.zkConn.Create(SERVICEREGISTERY+"/node_", metaByte, zk.FlagSequence|zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
+	if err != nil {
 		return fullPathString, err
 	}
 	fullPathString = PATH(fullPath)
 	return fullPathString, nil
 }
 
-func (sd *ServiceRegistry) RegisterServiceRegistryEvents(path string)error{
-	fmt.Println("RegisterServiceRegistryEvents")
-	childs, _, events, err := sd.zkConn.ChildrenW(path)
-	if err != nil{
-		return err
-	}
+func (sd *ServiceRegistry) RegisterServiceRegistryEvents(path string) error {
+	go func() {
+		fmt.Println("RegisterServiceRegistryEvents")
+		_, _, events, err := sd.zkConn.ChildrenW(path)
+		if err != nil {
+			fmt.Println(err, "error occured")
+		}
+		for {
+			select {
+			case event := <-events:
+				if event.Type == zk.EventNodeChildrenChanged {
+					fmt.Println("EventNodeChildrenChanged")
+					childs, _, _, err := sd.zkConn.ChildrenW(path)
+					if err != nil {
+						fmt.Println(err, "error occured")
+					}
+					sd.eventchangeLogs <- childs
 
-	for {
-		select {
-		case event := <-events:
-			if event.Type == zk.EventNodeChildrenChanged {
-				sd.eventchangeLogs <- childs
+				} else if event.Type == zk.EventNodeDataChanged {
+					childs, _, _, err := sd.zkConn.ChildrenW(path)
+					if err != nil {
+						fmt.Println(err, "error occured")
+					}
+					fmt.Println("EventNodeDataChanged")
+					sd.eventchangeLogs <- childs
 
-			} else if event.Type == zk.EventNodeDataChanged {
-				sd.eventchangeLogs <- childs
+				} else if event.Type == zk.EventNodeCreated {
+					childs, _, _, err := sd.zkConn.ChildrenW(path)
+					if err != nil {
+						fmt.Println(err, "error occured")
+					}
+					fmt.Println("EventNodeCreated")
+					sd.eventchangeLogs <- childs
 
-			} else if event.Type == zk.EventNodeCreated {
-				sd.eventchangeLogs <- childs
-
-			} else if event.Type == zk.EventNodeDeleted {
-				sd.eventchangeLogs <- childs
+				} else if event.Type == zk.EventNodeDeleted {
+					childs, _, _, err := sd.zkConn.ChildrenW(path)
+					if err != nil {
+						fmt.Println(err, "error occured")
+					}
+					fmt.Println("EventNodeDeleted")
+					sd.eventchangeLogs <- childs
+				}
 			}
 		}
-	}
+	}()
+	return nil
 }
 
-func (sd *ServiceRegistry) UpdateAddressList(childs []string){
-	for _, v := range childs{
-		zNodePath := SERVICEREGISTERY+"/"+v
+func (sd *ServiceRegistry) UpdateAddressList(childs []string) {
+	fmt.Println(childs)
+	for _, v := range childs {
+		zNodePath := SERVICEREGISTERY + "/" + v
 		exist, _, err := sd.zkConn.Exists(zNodePath)
-		if err != nil{
+		if err != nil {
 			continue
 		}
-		if !exist{
+		if !exist {
 			continue
 		}
 		data, _, err := sd.zkConn.Get(zNodePath)
-		if err != nil{
+		if err != nil {
 			continue
 		}
 		convertedData := string(data)
@@ -106,19 +129,22 @@ func (sd *ServiceRegistry) UpdateAddressList(childs []string){
 	}
 }
 
-func (sd *ServiceRegistry) ReRegisterServiceRegistryEvents(){
-	for{
-		select {
-		case data, ok := <- sd.eventchangeLogs:
-			if !ok {
-				continue
+func (sd *ServiceRegistry) ReRegisterServiceRegistryEvents() {
+	go func() {
+		fmt.Println("inside ReRegisterServiceRegistryEvents")
+		for {
+			select {
+			case data := <-sd.eventchangeLogs:
+				fmt.Println("Inside ReRegisterServiceRegistryEvents")
+				sd.UpdateAddressList(data)
+				_ = sd.RegisterServiceRegistryEvents(SERVICEREGISTERY)
 			}
-			sd.UpdateAddressList(data)
 		}
-	}
+	}()
+
 }
 
-func (sd *ServiceRegistry) ConnectZK() (<- chan zk.Event, error) {
+func (sd *ServiceRegistry) ConnectZK() (<-chan zk.Event, error) {
 	c, events, err := zk.Connect([]string{"127.0.0.1:2181"}, time.Second)
 	if err != nil {
 		return nil, nil
@@ -127,7 +153,7 @@ func (sd *ServiceRegistry) ConnectZK() (<- chan zk.Event, error) {
 	return events, nil
 }
 
-func (sd *ServiceRegistry) RegisterBaseEvents(events <-chan zk.Event){
+func (sd *ServiceRegistry) RegisterBaseEvents(events <-chan zk.Event) {
 	go func() {
 		isConnectedFlag := false
 		for !isConnectedFlag {
@@ -150,34 +176,31 @@ func (sd *ServiceRegistry) RegisterBaseEvents(events <-chan zk.Event){
 	}()
 }
 
-func main(){
+func main() {
 
 	sd := NewServiceRegistry()
 
 	events, err := sd.ConnectZK()
+	if err != nil {
+		return
+	}
+	err = sd.RegisterServiceRegistryEvents(SERVICEREGISTERY)
 	if err != nil{
 		return
 	}
 	sd.RegisterBaseEvents(events)
-
-
+	sd.ReRegisterServiceRegistryEvents()
 	_, err = sd.CreateServiceRegistryZnode()
-	if err != nil{
+	if err != nil {
+		return
+	}
+	_, err = sd.RegisterToCluster(SRMetaData{Endpoint: "127.0.0.1:2379"})
+	if err != nil {
 		return
 	}
 
 
-	srMeta := SRMetaData{Endpoint:"http://localhost:2380"}
-	path, err := sd.RegisterToCluster(srMeta)
-	if err != nil{
-		return
-	}
-    fmt.Println("fullPath", path)
-	go func(){
-		err := sd.RegisterServiceRegistryEvents(SERVICEREGISTERY+"/"+string(path))
-		if err != nil{
-			return
-		}
-	}()
+
+
 	<-sd.wait
 }
