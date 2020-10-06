71ed19eda169ca928fc5dcd84eb5d36a73c2dc1e
velann21 <velan@reynencourt.com>
2020-10-06 11:35:12 +0200

    Calling function CreateVPC in main

--- main.go
+++ main.go
@@ -4,90 +4,118 @@ import (
 	"fmt"
 	"github.com/go-zookeeper/zk"
 	"sort"
-	"strings"
+	"strconv"
 	"time"
 )
 
 var currentNode string
+
+
+
+
+
+
+
 func main() {
-	wait := make(chan bool)
-	nodeChangedEvent := make(chan string, 10)
-	registerWatcher := make(chan string, 10)
-	conn := CreateZookeeper(wait, registerWatcher)
 
-	path, err := conn.Create("/election/node_", []byte{}, zk.FlagSequence|zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
-	if err != nil {
-		fmt.Println(err)
-	}
-	node := strings.Split(path, "/")
-	currentNode = node[len(node)-1]
-	fmt.Println("current Node label ------->", node[len(node)-1])
-	master, err := IsMaster("/election", conn)
-	if err != nil{
-		fmt.Println(err)
-	}
-	if master == currentNode{
-		fmt.Println("Yes I am master")
-	}else{
-		childs, _, err := conn.Children("/election")
-		if err != nil{
-			fmt.Println("error")
-		}
-		sort.Strings(childs)
-		previous := ""
-		for _, v := range childs{
-			if v == currentNode{
-				break
-			}
-			previous = v
-		}
-		registerTo := previous
-		fmt.Println("sdsdsdsd","/election/"+registerTo)
-		AddWatcher("/election/"+registerTo, conn, nodeChangedEvent)
-		fmt.Println("No I am not master")
-	}
+	str := "xyz abc mnp  "
+
+    strconv.Atoi()
 
 
-	go func(){
-		for{
-			select {
-			case event := <- nodeChangedEvent:
-				fmt.Println("Events ----->>> :",event)
-				count := 0
-				for count <= 10{
-					master, err := IsMaster("/election", conn)
-					if err != nil{
-						fmt.Println(err)
-					}
-					if master == currentNode{
-						fmt.Println("Yes I am master")
-					}else{
-						childs, _, err := conn.Children("/election")
-						if err != nil{
-							fmt.Println("error")
-						}
-						sort.Strings(childs)
-						previous := ""
-						for _, v := range childs{
-							if v == currentNode{
-								break
-							}
-							previous = v
-						}
-						registerTo := previous
-						fmt.Println("sdsdsdsd","/election/"+registerTo)
-						AddWatcher("/election/"+registerTo, conn, nodeChangedEvent)
-						fmt.Println("No I am not master")
-					}
-					fmt.Println("Still master ----->>>>", master)
-					count++
-					time.Sleep(time.Second*10)
-				}
-			}
-		}
-	}()
 
-	<-wait
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//wait := make(chan bool)
+	//nodeChangedEvent := make(chan string, 10)
+	//registerWatcher := make(chan string, 10)
+	//conn := CreateZookeeper(wait, registerWatcher)
+	//
+	//path, err := conn.Create("/election/node_", []byte{}, zk.FlagSequence|zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//node := strings.Split(path, "/")
+	//currentNode = node[len(node)-1]
+	//fmt.Println("current Node label ------->", node[len(node)-1])
+	//master, err := IsMaster("/election", conn)
+	//if err != nil{
+	//	fmt.Println(err)
+	//}
+	//if master == currentNode{
+	//	fmt.Println("Yes I am master")
+	//}else{
+	//	childs, _, err := conn.Children("/election")
+	//	if err != nil{
+	//		fmt.Println("error")
+	//	}
+	//	sort.Strings(childs)
+	//	previous := ""
+	//	for _, v := range childs{
+	//		if v == currentNode{
+	//			break
+	//		}
+	//		previous = v
+	//	}
+	//	registerTo := previous
+	//	fmt.Println("sdsdsdsd","/election/"+registerTo)
+	//	AddWatcher("/election/"+registerTo, conn, nodeChangedEvent)
+	//	fmt.Println("No I am not master")
+	//}
+	//
+	//
+	//go func(){
+	//	for{
+	//		select {
+	//		case event := <- nodeChangedEvent:
+	//			fmt.Println("Events ----->>> :",event)
+	//			count := 0
+	//			for count <= 10{
+	//				master, err := IsMaster("/election", conn)
+	//				if err != nil{
+	//					fmt.Println(err)
+	//				}
+	//				if master == currentNode{
+	//					fmt.Println("Yes I am master")
+	//				}else{
+	//					childs, _, err := conn.Children("/election")
+	//					if err != nil{
+	//						fmt.Println("error")
+	//					}
+	//					sort.Strings(childs)
+	//					previous := ""
+	//					for _, v := range childs{
+	//						if v == currentNode{
+	//							break
+	//						}
+	//						previous = v
+	//					}
+	//					registerTo := previous
+	//					fmt.Println("sdsdsdsd","/election/"+registerTo)
+	//					AddWatcher("/election/"+registerTo, conn, nodeChangedEvent)
+	//					fmt.Println("No I am not master")
+	//				}
+	//				fmt.Println("Still master ----->>>>", master)
+	//				count++
+	//				time.Sleep(time.Second*10)
+	//			}
+	//		}
+	//	}
+	//}()
+	//
+	//<-wait
 }
 
 
