e56bab0b5ac37543e90b06c7d4ca6726527e889e
velann21 <velan@reynencourt.com>
2020-10-15 08:37:42 +0200

    Service discovery auto fail over added

--- distributed_workers_algo/pkg/leader_election_algo.go
+++ distributed_workers_algo/pkg/leader_election_algo.go
@@ -198,7 +198,6 @@ func main() {
 		log.Fatalln(err)
 	}
 
-
 	if leaderElection.CheckIfMaster(masterNode) {
 		RegisterAsMaster(masterNode, leaderElection.conn)
 	} else {
@@ -212,7 +211,7 @@ func main() {
 	<-wait
 }
 
-func RegisterAsMaster(node string, conn *zk.Conn){
+func RegisterAsMaster(node string, conn *zk.Conn) {
 	sd := NewServiceRegistry(conn)
 	type EP struct {
 		IP string
@@ -225,41 +224,48 @@ func RegisterAsMaster(node string, conn *zk.Conn){
 	}
 
 	isExist, err := sd.ZnodeExist(MASTERSERVICEREGISTERY)
-	if err != nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 
 	if !isExist {
-		_, err := sd.CreateZNode(MASTERSERVICEREGISTERY,[]byte{}, false, false)
+		_, err := sd.CreateZNode(MASTERSERVICEREGISTERY, []byte{}, false, false)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		_, err = sd.CreateZNode(MASTERSERVICEREGISTERY+"/"+node, metaByte, true, true)
+		_, err = sd.CreateZNode(MASTERSERVICEREGISTERY+"/"+node, metaByte, true, false)
 		if err != nil {
 			log.Fatalln(err)
 		}
-	}else{
+	} else {
 		childs, err := sd.GetZNodeChilds(MASTERSERVICEREGISTERY)
-		if err != nil{
+		if err != nil {
 			log.Fatalln(err)
 		}
 		if len(childs) > 0 {
-           for _, v := range childs{
-           	err := sd.DeleteZnode(MASTERSERVICEREGISTERY+"/"+v)
-           	if err != nil{
-				log.Fatalln(err)
+			for _, v := range childs {
+				err := sd.DeleteZnode(MASTERSERVICEREGISTERY + "/" + v)
+				if err != nil {
+					log.Fatalln(err)
+				}
 			}
-		   }
 		}
 
-		_, err = sd.CreateZNode(MASTERSERVICEREGISTERY+"/"+node, metaByte, true, true)
+		_, err = sd.CreateZNode(MASTERSERVICEREGISTERY+"/"+node, metaByte, true, false)
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
+
+	err = sd.WatchWorkerNodes(WORKERSERVICEREGISTERY)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sd.ReRegisterWatchWorkerNodes()
 }
 
-func RegisterAsWorker(node string, conn *zk.Conn){
+func RegisterAsWorker(node string, conn *zk.Conn) {
 	sd := NewServiceRegistry(conn)
 	type EP struct {
 		IP string
@@ -272,7 +278,7 @@ func RegisterAsWorker(node string, conn *zk.Conn){
 	}
 
 	isExist, err := sd.ZnodeExist(WORKERSERVICEREGISTERY)
-	if err != nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 
@@ -282,27 +288,26 @@ func RegisterAsWorker(node string, conn *zk.Conn){
 			log.Fatalln(err)
 		}
 		fmt.Println("Creating the worker node", WORKERSERVICEREGISTERY+"/"+node)
-		_, err = sd.CreateZNode(WORKERSERVICEREGISTERY+"/"+node, metaByte, true, true)
+		_, err = sd.CreateZNode(WORKERSERVICEREGISTERY+"/"+node, metaByte, true, false)
 		if err != nil {
 			log.Fatalln(err)
 		}
-	}else{
-		_, err = sd.CreateZNode(WORKERSERVICEREGISTERY+"/"+node, metaByte, true, true)
+	} else {
+		_, err = sd.CreateZNode(WORKERSERVICEREGISTERY+"/"+node, metaByte, true, false)
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
 
-
-
 }
 
-
 type PATH string
+
 const MASTERSERVICEREGISTERY = "/service_master"
 const WORKERSERVICEREGISTERY = "/service_workers"
+
 type ServiceRegistry struct {
-	conn *zk.Conn
+	conn            *zk.Conn
 	addressList     []string
 	eventchangeLogs chan []string
 	wait            chan bool
@@ -312,21 +317,39 @@ func NewServiceRegistry(conn *zk.Conn) *ServiceRegistry {
 	addressList := make([]string, 0)
 	eventchangeLogs := make(chan []string, 0)
 	wait := make(chan bool)
-	return &ServiceRegistry{addressList: addressList, eventchangeLogs: eventchangeLogs, wait: wait, conn:conn}
+	return &ServiceRegistry{addressList: addressList, eventchangeLogs: eventchangeLogs, wait: wait, conn: conn}
 }
 
 func (sd *ServiceRegistry) CreateZNode(path string, data []byte, ephmeral bool, seq bool) (PATH, error) {
 
 	var fullPathString PATH
-	if ephmeral == true && seq == true{
-		fullPath, err := sd.conn.Create(path, data, zk.FlagSequence | zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
+	if ephmeral == true && seq == true {
+		fmt.Println("ephmeral == true && seq == true")
+		fullPath, err := sd.conn.Create(path, data, zk.FlagSequence|zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
 		if err != nil {
 			return fullPathString, err
 		}
 		fullPathString = PATH(fullPath)
 		return fullPathString, nil
-	}else{
+	} else if ephmeral==false  && seq == false {
+		fmt.Println("ephmeral && seq == false")
 		fullPath, err := sd.conn.Create(path, data, 0, zk.WorldACL(zk.PermAll))
+		if err != nil {
+			return fullPathString, err
+		}
+		fullPathString = PATH(fullPath)
+		return fullPathString, nil
+	} else if ephmeral == true && seq == false {
+		fmt.Println("ephmeral == true && seq == false ")
+		fullPath, err := sd.conn.Create(path, data, zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
+		if err != nil {
+			return fullPathString, err
+		}
+		fullPathString = PATH(fullPath)
+		return fullPathString, nil
+	} else {
+		fmt.Println("Inside elase condit")
+		fullPath, err := sd.conn.Create(path, data, zk.FlagSequence, zk.WorldACL(zk.PermAll))
 		if err != nil {
 			return fullPathString, err
 		}
@@ -335,7 +358,6 @@ func (sd *ServiceRegistry) CreateZNode(path string, data []byte, ephmeral bool, 
 	}
 
 }
-
 
 func (sd *ServiceRegistry) GetZNodeChilds(path string) ([]string, error) {
 	childs, _, err := sd.conn.Children(path)
@@ -345,7 +367,7 @@ func (sd *ServiceRegistry) GetZNodeChilds(path string) ([]string, error) {
 	return childs, nil
 }
 
-func (sd *ServiceRegistry) DeleteZNode(path string, version int32) (error) {
+func (sd *ServiceRegistry) DeleteZNode(path string, version int32) error {
 	err := sd.conn.Delete(path, version)
 	if err != nil {
 		return err
@@ -353,25 +375,24 @@ func (sd *ServiceRegistry) DeleteZNode(path string, version int32) (error) {
 	return nil
 }
 
-
-func (sd *ServiceRegistry) ZnodeExist(path string)(bool,error){
+func (sd *ServiceRegistry) ZnodeExist(path string) (bool, error) {
 	exist, _, err := sd.conn.Exists(path)
 	if err != nil {
-		return  false, err
+		return false, err
 	}
 	return exist, nil
 }
 
-func (sd *ServiceRegistry) DeleteZnode(path string)(error){
+func (sd *ServiceRegistry) DeleteZnode(path string) error {
 	err := sd.conn.Delete(path, 0)
 	if err != nil {
-		return  err
+		return err
 	}
 	return nil
 }
 
 func (sd *ServiceRegistry) UpdateAddressList(childs []string) {
-	fmt.Println(childs)
+	fmt.Println("UpdateAddressList childs---->>", childs)
 	for _, v := range childs {
 		zNodePath := WORKERSERVICEREGISTERY + "/" + v
 		exist, _, err := sd.conn.Exists(zNodePath)
@@ -453,5 +474,3 @@ func (sd *ServiceRegistry) WatchWorkerNodes(path string) error {
 	}()
 	return nil
 }
-
-
